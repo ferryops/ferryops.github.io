bebedc10392d1d075093471a0cbb9a52d057ffea
ferryops <ferry.a.febian@gmail.com>
2024-09-14 14:36:46 +0800

    fix tail

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Navigation />
       <div className="flex flex-col w-full gap-2">
         <Typography variant="h2" size="md" className="md:max-w-3xl">
-          I'm a software engineer that rarely writes code.
+          I am a software engineer that rarely writes code.
         </Typography>
         <Typography variant="p" size="sm" className="md:max-w-3xl">
           Meet Manu Arora, the self-proclaimed code wizard who can turn caffeine into beautiful websites. His passion for building
